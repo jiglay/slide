129d7978f42804802d4ce2d8bd2789296a01d124
Jiglay <jiglay@MacBook-Pro-de-Jiglay.local>
2021-05-25 15:42:59 -0300

    config index dos slides

--- js/slide.js
+++ js/slide.js
@@ -40,7 +40,8 @@ export default class Slide {
     }
 
     onEnd(event) {
-        this.wrapper.removeEventListener('mousemove', this.onMove);
+        const moveType = (event.type === 'mouseup') ? 'mousemove' : 'touchmove';
+        this.wrapper.removeEventListener(moveType, this.onMove);
         this.dist.finalPosition = this.dist.movePosition;
     }
 
@@ -58,9 +59,42 @@ export default class Slide {
         this.onEnd = this.onEnd.bind(this);
     }
 
+    // slides config
+
+    slidePosition(slide) {
+        // margin para a imagem ficar exatamente no centro, independente do tamanho de tela
+        const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
+        return -(slide.offsetLeft - margin);
+    }
+
+    slidesConfig() {
+        this.slideArray = [...this.slide.children].map(element => {
+            const position = this.slidePosition(element);
+            return { position, element };
+        });
+    }
+
+    slidesIndexNav(index) {
+        const last = this.slideArray.length - 1;
+        this.index = {
+            prev: index ? index - 1 : undefined,
+            active: index,
+            next: (index === last) ? undefined : (index + 1)
+        };
+    }
+
+    changeSlide(index) {
+        const activeSlide = this.slideArray[index];
+
+        this.moveSlide(activeSlide.position);
+        this.slidesIndexNav(index);
+        this.dist.finalPosition = activeSlide.position;
+    }
+
     init() {
         this.bindEvents();
         this.addSlideEvents();
+        this.slidesConfig();
         return this;
     }
 }
